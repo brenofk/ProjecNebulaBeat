875aeecf7ca5d72e6a0965ad824648a7a1ab3c75
brenofk <brenofernandosoaresa.d.s@gmail.com>
2024-08-30 19:23:27 -0300

    add no Models: Classe PlayList & Comentarios.

--- Musicas/models.py
+++ Musicas/models.py
@@ -3,11 +3,26 @@ from django.db import models
 # Create your models here.
 
 class Musicas(models.Model):
-    titulo = models.CharField(max_length=200)
-    artista = models.CharField(max_length=200)
-    album = models.CharField(max_length=200)
-    genero = models.CharField(max_length=200)
-    url_reproducao = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100)
+    artista = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    genero = models.CharField(max_length=100)
+    url_reproducao = models.CharField(max_length=100)
 
-def __str__(self):
-    return self.titulo
+    def __str__(self):
+        return self.titulo
+
+class Playlist(models.Model):
+    nome = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+class Comentario(models.Model):
+    nota = models.CharField(max_length=10)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.nota
+
